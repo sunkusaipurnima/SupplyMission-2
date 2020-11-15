4779902633ca090f9ba1dd73409e0a15fd8b5510
Sunku Sai Purnima <69891257+sunkusaipurnima@users.noreply.github.com>
2020-11-15 19:09:12 +0530

    Update box.js
    corrected this.body options in constructor.. working fine

--- box.js
+++ box.js
@@ -3,7 +3,7 @@ class Box{
        var options={
            isStatic : true
        }
-       this.body= Bodies.rectangle(x,y,width,height,options);
+       this.body= Bodies.rectangle(x,y,w,h,options);
        World.add(world,this.body);
        this.width=w;
        this.height=h;
